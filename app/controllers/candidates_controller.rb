d50865613f51b9f44e69df74f3758a7893a5b20a
Caroline Ibbotson <carolinem.ibb@gmail.com>
2020-12-10 19:38:26 -0500

    Controller updates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 class CandidatesController < ApplicationController
-  before_action :set_candidate, only: [:show, :edit, :update, :destroy, :vote]
+  before_action :set_candidate, only: %i[show edit update destroy vote]
+  before_action :verify_owner, only: %i[edit update destroy]
 
   # GET /candidates
   # GET /candidates.json
   def index
-    @candidates = Candidate.all.order(last_name: :desc)
-  end
-
-  # GET /candidates/1
-  # GET /candidates/1.json
-  def show
+    @candidate =
+      if helpers.voting_over?
+        Candidate.all.order(vote_count: :desc)
+      else
+        Candidate.all.order(last_name: :desc)
+      end
   end
 
   # GET /candidates/new
@@ -18,14 +21,6 @@ class CandidatesController < ApplicationController
       @candidate = Candidate.new
     else
       flash[:warning] = 'Nominations are not currently open.'
-      redirect_to candidates_path    
-    end
-  end
-
-  # GET /candidates/1/edit
-  def edit
-    unless @candidate.user_id == current_user.id
-      flash[:warning] = 'You can only edit your own candidacy.'
       redirect_to candidates_path
     end
   end
@@ -46,26 +41,6 @@ class CandidatesController < ApplicationController
     end
   end
 
-  def vote
-    if helpers.within_voting_period?
-      if current_user.votes.count > 5
-        redirect_to candidates_path, notice: "You've hit your maximum (5) number of allowed votes."
-      else
-        vote = current_user.votes.find_by(candidate: @candidate)
-        if vote.present?
-          vote.destroy
-          redirect_to candidates_path, notice: "You have removed your vote."
-        else
-          current_user.votes.create!(candidate: @candidate)
-          redirect_to candidates_path, notice: "You voted! Yay!"
-        end
-      end
-    else
-      flash[:warning] = 'Voting is not currently open; you cannot vote.'
-      redirect_to candidates_path
-    end
-  end
-
   # PATCH/PUT /candidates/1
   # PATCH/PUT /candidates/1.json
   def update
@@ -83,29 +58,51 @@ class CandidatesController < ApplicationController
   # DELETE /candidates/1
   # DELETE /candidates/1.json
   def destroy
-    if @candidate.user_id == current_user.id
-        @candidate.destroy
-      respond_to do |format|
-        format.html { redirect_to candidates_url, 
-          notice: 'Candidate was successfully destroyed.' }
-        format.json { head :no_content }
+    @candidate.destroy
+    respond_to do |format|
+      format.html { redirect_to candidates_url, notice: 'Candidate was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  # POST /candidates/1/vote
+  def vote
+    if helpers.within_voting_period?
+      if current_user.votes.count > 5
+        redirect_to candidates_path, notice: "You've hit your maximum (5) number of allowed votes."
+      else
+        vote = current_user.votes.find_by(candidate: @candidate)
+        if vote.present?
+          vote.destroy!
+          redirect_to candidates_path, notice: 'You have removed your vote.'
+        else
+          current_user.votes.create!(candidate: @candidate)
+          redirect_to candidates_path, notice: 'You voted! Yay!'
+        end
       end
     else
-      flash[:warning] = "You cannot destroy another's candidacy."
+      flash[:warning] = 'Voting is not currently open; you cannot vote.'
       redirect_to candidates_path
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_candidate
-      @candidate = Candidate.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def candidate_params
-      params.require(:candidate)
-        .permit(:first_name, :last_name, :bio, :professional, :picture, :why)
-        .merge(user_id: current_user.id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_candidate
+    @candidate = Candidate.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def candidate_params
+    params.require(:candidate)
+          .permit(:first_name, :last_name, :bio, :professional, :picture, :why)
+          .merge(user_id: current_user.id)
+  end
+
+  def verify_owner
+    return @candidate.user_id == current_user.id
+    flash[:warning] = 'You can only make changes to your own candidacy.'
+    redirect_to candidates_path
+  end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def slack
     @user = User.from_omniauth(request.env['omniauth.auth'])
